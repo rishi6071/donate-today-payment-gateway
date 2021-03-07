03d7a3ccbc4d067b2d6eae7d79787f5152bbc722
Rishi Soni <rishisoni6071@gmail.com>
2021-03-07 13:56:05 +0530

    Added checkWithDoAnime() for Animation Performance

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+// Animation Changing according to width
+const checkWidthDoAnime = () => {
+    // console.log(window.outerWidth);
+    if (window.outerWidth <= 768) {
+        const dataAos = document.body.querySelectorAll('[data-aos]');
+        for (let i = 0; i < dataAos.length; i++) {
+            dataAos[i].setAttribute('data-aos', 'fade-up');
+        }
+    }
+}
+
+// Contact Form
 const contactUs = (event) => {
 
     const userName = document.querySelector('#contactName');
